0c9108de03d7c4257c070b06693b30212b6a1756
Przemek <p.bachorek@gmail.com>
2018-08-20 14:10:42 +0200

    Added: One, specialized method for printing html code

--- rddg-breadcrumbs.php
+++ rddg-breadcrumbs.php
@@ -3,7 +3,7 @@
 Plugin Name: RDDG Breadcrumbs
 Plugin URI: https://pb-86.github.io/RDDG-breadcrumbs/
 Description: Simple and lightweight plugin for theme developers that provide easy to use function for displaying breadcrumbs.
-Version: 1.1
+Version: 1.1.1
 Author: Przemek Bąchorek
 Author URI: https://reddog.systems
 License: GPLv2 or later
@@ -36,212 +36,124 @@ $position = 1;
  * @return void|null
  */
 function rddgbc() {
-  $tag_templates = array(
-    'opening_tag'   => '<nav class="rddgbc" aria-label="breadcrumb"><ol class="rddgbc__list" itemscope itemtype="http://schema.org/BreadcrumbList">',
-    'closing_tag'   => '</ol></nav>',
-    'list_opening'  => '<li class="rddgbc__item" itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">',
-    'list_current'  => '<li class="rddgbc__item rddgbc__item--active" aria-current="page" itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">',
-    'list_closing'  => '</li>',
-    'link_class'    => 'rddgbc__link'
-  );
-
-  if( !is_front_page() ) {
-    echo $tag_templates['opening_tag'];
-    rddgbc_the_home( $tag_templates );
-
-    if( is_singular() )
+  $container_opened = '<nav class="rddgbc" aria-label="breadcrumb">';
+  $container_closed = '</nav>';
+  $list_opened      = '<ol class="rddgbc__list" itemscope itemtype="http://schema.org/BreadcrumbList">';
+  $list_closed      = '</ol>';
+  if ( !is_front_page() ) {
+    echo $container_opened;
+    echo $list_opened;
+    rddgbc_the_home();
+    if ( is_singular() )
       rddgbc_the_singular( $tag_templates );
-    elseif( is_archive() )
+    elseif ( is_archive() )
       rddgbc_the_archive( $tag_templates );
-    elseif( is_search() )
+    elseif ( is_search() )
       rddgbc_the_search( $tag_templates );
-    elseif( is_404() )
+    elseif ( is_404() )
       rddgbc_the_404( $tag_templates );
-
-    echo $tag_templates['closing_tag'];
+    echo $list_closed;
+    echo $container_closed;
   }
 }
 
 /**
  * This method prints link to the home page.
- * @param  array $tag_templates
- * @return void
+ * @return void|null
  */
-function rddgbc_the_home( $tag_templates ) {
-  extract( $tag_templates );
-  $url      = esc_url( home_url( '/' ) );
-  $title    = esc_html__( 'Home page', 'rddgbc' );
-  $position = rddgbc_get_position();
-  $html     = "{$list_opening}"
-            . "<a class=\"{$link_class}\" href=\"{$url}\" itemprop=\"item\" itemtype=\"http://schema.org/Thing\">"
-            . "<span itemprop=\"name\">{$title}</span>"
-            . "</a>"
-            . "{$position}"
-            . "{$list_closing}";
-  echo $html;
+function rddgbc_the_home() {
+  $url    = esc_url( home_url( '/' ) );
+  $title  = esc_html__( 'Home page', 'rddgbc' );
+  rddgbc_print( $url, $title );
 }
 
 /**
  * This method prints crumb with title of 404 error page.
- * @param  array $tag_templates
- * @return void
+ * @return void|null
  */
-function rddgbc_the_404( $tag_templates ) {
-  extract( $tag_templates );
-  $url      = get_permalink();
-  $title    = esc_html__( '404', 'rddgbc' );
-  $position = rddgbc_get_position();
-  $html     = "{$list_current}"
-            . "<a href=\"{$url}\" itemprop=\"item\" itemtype=\"http://schema.org/Thing\">"
-            . "<span itemprop=\"name\">{$title}</span>"
-            . "</a>"
-            . "{$position}"
-            . "{$list_closing}";
-  echo $html;
+function rddgbc_the_404() {
+  $url    = get_permalink();
+  $title  = esc_html__( '404', 'rddgbc' );
+  rddgbc_print( $url, $title, true );
 }
 
 /**
  * This method prints crumb with title of search page
- * @param  array $tag_templates
- * @return void
+ * @return void|null
  */
-function rddgbc_the_search( $tag_templates ) {
-  extract( $tag_templates );
-  $url      = get_search_link();
-  $title    = esc_html__( 'Search result for: ' . get_search_query(), 'rddgbc' );
-  $position = rddgbc_get_position();
-  $html     = "{$list_current}"
-            . "<a href=\"{$url}\" itemprop=\"item\" itemtype=\"http://schema.org/Thing\">"
-            . "<span itemprop=\"name\">{$title}</span>"
-            . "</a>"
-            . "{$position}"
-            . "{$list_closing}";
-  echo $html;
+function rddgbc_the_search() {
+  $url    = get_search_link();
+  $title  = esc_html__( 'Search result for: ' . get_search_query(), 'rddgbc' );
+  rddgbc_print( $url, $title, true );
 }
 
 /**
  * This method prints current category and its ancestors.
- * @return void
+ * @return void|null
  */
-function rddgbc_the_archive( $tag_templates ) {
-  extract( $tag_templates );
+function rddgbc_the_archive() {
   $current_category_id = get_query_var('cat');
-
   $category_ancestors = array_reverse( get_ancestors( $current_category_id, 'category' ) );
-  if( $category_ancestors ) {
-    foreach( $category_ancestors as $category_ancestor_id ) {
-      $category_ancestor_position = rddgbc_get_position();
-      $category_ancestor_url      = get_category_link( $category_ancestor_id );
-      $category_ancestor_title    = get_cat_name( $category_ancestor_id );
-      $category_ancestor_html     = "{$list_opening}"
-                                  . "<a class=\"{$link_class}\" href=\"{$category_ancestor_url}\" itemprop=\"item\" itemtype=\"http://schema.org/Thing\">"
-                                  . "<span itemprop=\"name\">{$category_ancestor_title}</span>"
-                                  . "</a>"
-                                  . "{$category_ancestor_position}"
-                                  . "{$list_closing}";
-      echo $category_ancestor_html;
+  if ( $category_ancestors ) {
+    foreach ( $category_ancestors as $ancestor_id ) {
+      $ancestor_url    = get_category_link( $ancestor_id );
+      $ancestor_title  = get_cat_name( $ancestor_id );
+      rddgbc_print( $ancestor_url, $ancestor_title );
     }
   }
-
-  $current_category_position  = rddgbc_get_position();
-  $current_category_url = get_category_link( $current_category_id );
-  $current_category_title     = get_cat_name( $current_category_id );
-  $current_category_html  = "{$list_current}"
-                          . "<a href=\"{$current_category_url}\" itemprop=\"item\" itemtype=\"http://schema.org/Thing\">"
-                          . "<span itemprop=\"name\">{$current_category_title}</span>"
-                          . "</a>"
-                          . "{$current_category_position}"
-                          . "{$list_closing}";
-  echo $current_category_html;
+  $current_category_url   = get_category_link( $current_category_id );
+  $current_category_title = get_cat_name( $current_category_id );
+  rddgbc_print( $current_category_url, $current_category_title, true );
 }
 
 /**
  * This method prints page ancestors or post category hierarchy depending of
  * type of the singular and the title of current post or page.
- * @param  array $tag_templates
- * @return void
+ * @return void|null
  */
-function rddgbc_the_singular( $tag_templates ) {
-  extract( $tag_templates );
-
-  if( is_page() )
-    rddgbc_the_page_ancestors( $tag_templates );
+function rddgbc_the_singular() {
+  if ( is_page() )
+    rddgbc_the_page_ancestors();
   elseif ( is_single() )
-    rddgbc_the_categories( $tag_templates );
-
-  $url      = get_permalink();
-  $title    = get_the_title();
-  $position = rddgbc_get_position();
-  $html     = "{$list_current}"
-            . "<a href=\"{$url}\" itemprop=\"item\" itemtype=\"http://schema.org/Thing\">"
-            . "<span itemprop=\"name\">{$title}</span>"
-            . "</a>"
-            . "{$position}"
-            . "{$list_closing}";
-  echo $html;
+    rddgbc_the_categories();
+  $url    = get_permalink();
+  $title  = get_the_title();
+  rddgbc_print( $url, $title, true );
 }
 
 /**
  * This method prints all the ancestors for the current page.
- * @param  array $tag_templates
- * @return void
+ * @return void|null
  */
-function rddgbc_the_page_ancestors( $tag_templates ) {
-  extract( $tag_templates );
+function rddgbc_the_page_ancestors() {
   $ancestors = array_reverse( get_ancestors( get_the_ID(), 'page' ) );
-
-  if( $ancestors ) {
-    foreach( $ancestors as $ancestor_id ) {
-      $position       = rddgbc_get_position();
-      $ancestor_url   = get_page_link( $ancestor_id );
-      $ancestor_title = get_the_title( $ancestor_id );
-      $ancestor_html  = "{$list_opening}"
-                      . "<a class=\"{$link_class}\" href=\"{$ancestor_url}\" itemprop=\"item\" itemtype=\"http://schema.org/Thing\">"
-                      . "<span itemprop=\"name\">{$ancestor_title}</span>"
-                      . "</a>"
-                      . "{$position}"
-                      . "{$list_closing}";
-      echo $ancestor_html;
+  if ( $ancestors ) {
+    foreach ( $ancestors as $id ) {
+      $url   = get_page_link( $id );
+      $title = get_the_title( $id );
+      rddgbc_print( $url, $title );
     }
   }
 }
 
 /**
  * This method prints main category and its ancestors for the current post.
- * @param  array $tag_templates
- * @return void
+ * @return void|null
  */
-function rddgbc_the_categories( $tag_templates ) {
-  extract( $tag_templates );
+function rddgbc_the_categories() {
   $categories = wp_get_post_categories( get_the_ID() );
-  $main_category_id = $categories[0];
-
-  $category_ancestors = get_ancestors( $main_category_id, 'category' );
-  if( $category_ancestors ) {
-    foreach( $category_ancestors as $category_ancestor_id ) {
-      $position                 = rddgbc_get_position();
-      $category_ancestor_url    = get_category_link( $category_ancestor_id );
-      $category_ancestor_title  = get_cat_name( $category_ancestor_id );
-      $category_ancestor_html   = "{$list_opening}"
-                                . "<a class=\"{$link_class}\" href=\"{$category_ancestor_url}\" itemprop=\"item\" itemtype=\"http://schema.org/Thing\">"
-                                . "<span itemprop=\"name\">{$category_ancestor_title}</span>"
-                                . "</a>"
-                                . "{$position}"
-                                . "{$list_closing}";
-      echo $category_ancestor_html;
+  $category_id = $categories[0];
+  $category_ancestors = get_ancestors( $category_id, 'category' );
+  if ( $category_ancestors ) {
+    foreach ( $category_ancestors as $ancestor_id ) {
+      $ancestor_url   = get_category_link( $ancestor_id );
+      $ancestor_title = get_cat_name( $ancestor_id );
+      rddgbc_print( $ancestor_url, $ancestor_title );
     }
   }
-
-  $main_category_position = rddgbc_get_position();
-  $main_category_url      = get_category_link( $main_category_id );
-  $main_category_title    = get_cat_name( $main_category_id );
-  $main_category_html     = "{$list_opening}"
-                          . "<a class=\"{$link_class}\" href=\"{$main_category_url}\" itemprop=\"item\" itemtype=\"http://schema.org/Thing\">"
-                          . "<span itemprop=\"name\">{$main_category_title}</span>"
-                          . "</a>"
-                          . "{$main_category_position}"
-                          . "{$list_closing}";
-  echo $main_category_html;
+  $category_url   = get_category_link( $category_id );
+  $category_title = get_cat_name( $category_id );
+  rddgbc_print( $category_url, $category_title, true );
 }
 
 /**
@@ -254,4 +166,26 @@ function rddgbc_get_position() {
   $position_html    = "<meta itemprop=\"position\" content=\"{$position_counter}\">";
   $GLOBALS['position']++;
   return $position_html;
+}
+
+/**
+ * This method echoes current list item according to the following pattern
+ * @param  string  $url
+ * @param  string  $title
+ * @param  boolean $is_last
+ * @return void
+ */
+function rddgbc_print( $url, $title, $is_last = false ) {
+  $li_opened    = '<li class="rddgbc__item" itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">';
+  $li_last      = '<li class="rddgbc__item rddgbc__item--active" aria-current="page" itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">';
+  $a_opened     = "<a class=\"rddgbc__link\" href=\"{$url}\" itemprop=\"item\" itemtype=\"http://schema.org/Thing\">";
+  $span_opened  = "<span itemprop=\"name\">";
+  $span_closed  = "</span>";
+  $a_closed     = '</a>';
+  $position     = rddgbc_get_position();
+  $li_closed    = '</li>';
+  if ( true == $is_last )
+    echo "{$li_last}{$a_opened}{$span_opened}{$title}{$span_closed}{$a_closed}{$position}{$li_closed}";
+  else
+    echo "{$li_opened}{$a_opened}{$span_opened}{$title}{$span_closed}{$a_closed}{$position}{$li_closed}";
 }
