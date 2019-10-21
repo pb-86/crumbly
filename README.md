d853b0df714ff992b9b2a3ecd608e34799a1ae93
Przemek Bąchorek <przemek@reddog.systems>
2019-10-21 14:39:33 +0200

    Added: Few minor changes

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# RDDG Breadcrumbs
+# RDDG-breadcrumbs
 Simple and lightweight [WordPress](https://wordpress.org/) plugin for theme developers that provide easy to use function for displaying breadcrumbs.
 
 Please keep in mind that this is my very first WordPress plugin. I will make every effort to ensure that it is constantly developed. All comments and advice are of course welcome.
 
 ## How to use it
-To display breadcrumbs on your page using **RDDG Breadcrumbs** plugin, you have to simply add following line of code in your WordPress theme. Inserting it to the header.php file will be the best way.
+To display breadcrumbs on your page using **RDDG-breadcrumbs** plugin, you have to simply add following line of code in your WordPress theme. Inserting it to the header.php file will be the best way.
 ```php
 <?php if( function_exists( 'rddgbc' ) ) { rddgbc(); } ?>
 ```
@@ -18,6 +18,7 @@ In short, this is the case:
       <a class="rddgbc__link" href="" itemprop="item" itemtype="http://schema.org/Thing">
         <span itemprop="name">Home page</span>
       </a>
+      &raquo;
       <meta itemprop="position" content="1">
     </li>
     <li class="rddgbc__item  rddgbc__item--active" aria-current="page" itemscope="" itemprop="itemListElement" itemtype="http://schema.org/ListItem">
@@ -31,7 +32,7 @@ In short, this is the case:
 ```
 
 ## Add your CSS
-In default, **RDDG Breadcrumbs** will only print your breadcrumbs in form of ordered list. To make your breadcrumbs look great, you have to add some CSS styles.
+In default, **RDDG-breadcrumbs** will only print your breadcrumbs in form of ordered list. To make your breadcrumbs look great, you have to add some CSS styles.
 
 So, I recommend to start with following CSS:
 ```css
@@ -50,6 +51,9 @@ So, I recommend to start with following CSS:
 .rddgbc__link {
   /* This is the class that is added to all the links */
 }
+.rddgbc__separator {
+  /* This is the class that is added to separator container tag */
+}
 ```
 
 or if you prefer [Sass](https://sass-lang.com/) with [BEM](http://getbem.com/) methodology:
@@ -67,6 +71,9 @@ or if you prefer [Sass](https://sass-lang.com/) with [BEM](http://getbem.com/) m
   }
   &__link {
     // This is the class that is added to all the links
+  }
+  &__separator {
+  /* This is the class that is added to separator container tag */
   }
 }
 ```
--- rddg-breadcrumbs.php
+++ rddg-breadcrumbs.php
@@ -1,199 +1,214 @@
 <?php
-/*
-Plugin Name: RDDG Breadcrumbs
-Plugin URI: https://pb-86.github.io/RDDG-breadcrumbs/
-Description: Simple and lightweight plugin for theme developers that provide easy to use function for displaying breadcrumbs.
-Version: 1.2
-Author: Przemek Bąchorek
-Author URI: https://reddog.systems
-License: GPLv2 or later
-License URI: https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain: rddgbc
-Domain Path: /languages
-
-{Plugin Name} is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-any later version.
-
-{Plugin Name} is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with {Plugin Name}. If not, see {URI to Plugin License}.
-*/
+/**
+ * Plugin Name: RDDG Breadcrumbs
+ * Plugin URI: https://pb-86.github.io/RDDG-breadcrumbs/
+ * Description: Simple and lightweight plugin for theme developers that provide easy to use function for displaying breadcrumbs.
+ * Version: 1.3
+ * Author: Przemek Bąchorek
+ * Author URI: https://reddog.systems
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: rddgbc
+ * Domain Path: /languages
+ *
+ * RDDG Breadcrumbs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * RDDG Breadcrumbs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RDDG Breadcrumbs.
+ */
 
 /**
  * Loading the translation files
  */
-add_action( 'plugins_loaded', 'rddgbc_load_textdomain' );
 function rddgbc_load_textdomain() {
-  load_plugin_textdomain( 'rddgbc', false, basename( dirname( __FILE__ ) ) . '/languages' );
+	load_plugin_textdomain( 'rddgbc', false, basename( dirname( __FILE__ ) ) . '/languages' );
 }
+add_action( 'plugins_loaded', 'rddgbc_load_textdomain' );
 
 /**
  * Variable in which the position of the crumble is stored
+ *
  * @var integer
  */
 $position = 1;
 
 /**
  * This is main method of the plugin
+ *
  * @return void|null
  */
 function rddgbc() {
-  $container_opened = '<nav class="rddgbc" aria-label="breadcrumb">';
-  $container_closed = '</nav>';
-  $list_opened      = '<ol class="rddgbc__list" itemscope itemtype="http://schema.org/BreadcrumbList">';
-  $list_closed      = '</ol>';
-  if ( !is_front_page() ) {
-    echo $container_opened;
-    echo $list_opened;
-    rddgbc_the_home();
-    if ( is_singular() )
-      rddgbc_the_singular();
-    elseif ( is_archive() )
-      rddgbc_the_archive();
-    elseif ( is_search() )
-      rddgbc_the_search();
-    elseif ( is_404() )
-      rddgbc_the_404();
-    echo $list_closed;
-    echo $container_closed;
-  }
+	$container_opened = '<nav class="rddgbc" aria-label="breadcrumb">';
+	$container_closed = '</nav>';
+	$list_opened      = '<ol class="rddgbc__list" itemscope itemtype="http://schema.org/BreadcrumbList">';
+	$list_closed      = '</ol>';
+	if ( ! is_front_page() ) {
+		echo $container_opened;
+		echo $list_opened;
+		rddgbc_the_home();
+		if ( is_singular() ) {
+			rddgbc_the_singular();
+		} elseif ( is_archive() ) {
+			rddgbc_the_archive();
+		} elseif ( is_search() ) {
+			rddgbc_the_search();
+		} elseif ( is_404() ) {
+			rddgbc_the_404();
+		}
+		echo $list_closed;
+		echo $container_closed;
+	}
 }
 
 /**
  * This method prints link to the home page.
+ *
  * @return void|null
  */
 function rddgbc_the_home() {
-  $url    = esc_url( home_url( '/' ) );
-  $title  = esc_html__( 'Home page', 'rddgbc' );
-  rddgbc_print( $url, $title );
+	$url   = esc_url( home_url( '/' ) );
+	$title = esc_html__( 'Home page', 'rddgbc' );
+	rddgbc_print( $url, $title );
 }
 
 /**
  * This method prints crumb with title of 404 error page.
+ *
  * @return void|null
  */
 function rddgbc_the_404() {
-  $url    = get_permalink();
-  $title  = esc_html__( 'Error 404 - Page not found', 'rddgbc' );
-  rddgbc_print( $url, $title, true );
+	$url   = get_permalink();
+	$title = esc_html__( 'Error 404 - Page not found', 'rddgbc' );
+	rddgbc_print( $url, $title, true );
 }
 
 /**
  * This method prints crumb with title of search page
+ *
  * @return void|null
  */
 function rddgbc_the_search() {
-  $url    = get_search_link();
-  $title  = esc_html__( 'Search result for: ' . get_search_query(), 'rddgbc' );
-  rddgbc_print( $url, $title, true );
+	$url   = get_search_link();
+	$title = esc_html__( 'Search result for: ', 'rddgbc' ) . get_search_query();
+	rddgbc_print( $url, $title, true );
 }
 
 /**
  * This method prints current category and its ancestors.
+ *
  * @return void|null
  */
 function rddgbc_the_archive() {
-  $current_category_id = get_query_var('cat');
-  $category_ancestors = array_reverse( get_ancestors( $current_category_id, 'category' ) );
-  if ( $category_ancestors ) {
-    foreach ( $category_ancestors as $ancestor_id ) {
-      $ancestor_url    = get_category_link( $ancestor_id );
-      $ancestor_title  = get_cat_name( $ancestor_id );
-      rddgbc_print( $ancestor_url, $ancestor_title );
-    }
-  }
-  $current_category_url   = get_category_link( $current_category_id );
-  $current_category_title = get_cat_name( $current_category_id );
-  rddgbc_print( $current_category_url, $current_category_title, true );
+	$current_category_id = get_query_var( 'cat' );
+	$category_ancestors  = array_reverse( get_ancestors( $current_category_id, 'category' ) );
+	if ( $category_ancestors ) {
+		foreach ( $category_ancestors as $ancestor_id ) {
+			$ancestor_url   = get_category_link( $ancestor_id );
+			$ancestor_title = get_cat_name( $ancestor_id );
+			rddgbc_print( $ancestor_url, $ancestor_title );
+		}
+	}
+	$current_category_url   = get_category_link( $current_category_id );
+	$current_category_title = get_cat_name( $current_category_id );
+	rddgbc_print( $current_category_url, $current_category_title, true );
 }
 
 /**
  * This method prints page ancestors or post category hierarchy depending of
  * type of the singular and the title of current post or page.
+ *
  * @return void|null
  */
 function rddgbc_the_singular() {
-  if ( is_page() )
-    rddgbc_the_page_ancestors();
-  elseif ( is_single() )
-    rddgbc_the_categories();
-  $url    = get_permalink();
-  $title  = get_the_title();
-  rddgbc_print( $url, $title, true );
+	if ( is_page() ) {
+		rddgbc_the_page_ancestors();
+	} elseif ( is_single() ) {
+		rddgbc_the_categories();
+	}
+	$url   = get_permalink();
+	$title = get_the_title();
+	rddgbc_print( $url, $title, true );
 }
 
 /**
  * This method prints all the ancestors for the current page.
+ *
  * @return void|null
  */
 function rddgbc_the_page_ancestors() {
-  $ancestors = array_reverse( get_ancestors( get_the_ID(), 'page' ) );
-  if ( $ancestors ) {
-    foreach ( $ancestors as $id ) {
-      $url   = get_page_link( $id );
-      $title = get_the_title( $id );
-      rddgbc_print( $url, $title );
-    }
-  }
+	$ancestors = array_reverse( get_ancestors( get_the_ID(), 'page' ) );
+	if ( $ancestors ) {
+		foreach ( $ancestors as $id ) {
+			$url   = get_page_link( $id );
+			$title = get_the_title( $id );
+			rddgbc_print( $url, $title );
+		}
+	}
 }
 
 /**
  * This method prints main category and its ancestors for the current post.
+ *
  * @return void|null
  */
 function rddgbc_the_categories() {
-  $categories = wp_get_post_categories( get_the_ID() );
-  $category_id = $categories[0];
-  $category_ancestors = get_ancestors( $category_id, 'category' );
-  if ( $category_ancestors ) {
-    foreach ( $category_ancestors as $ancestor_id ) {
-      $ancestor_url   = get_category_link( $ancestor_id );
-      $ancestor_title = get_cat_name( $ancestor_id );
-      rddgbc_print( $ancestor_url, $ancestor_title );
-    }
-  }
-  $category_url   = get_category_link( $category_id );
-  $category_title = get_cat_name( $category_id );
-  rddgbc_print( $category_url, $category_title, true );
+	$categories         = wp_get_post_categories( get_the_ID() );
+	$category_id        = $categories[0];
+	$category_ancestors = get_ancestors( $category_id, 'category' );
+	if ( $category_ancestors ) {
+		foreach ( $category_ancestors as $ancestor_id ) {
+			$ancestor_url   = get_category_link( $ancestor_id );
+			$ancestor_title = get_cat_name( $ancestor_id );
+			rddgbc_print( $ancestor_url, $ancestor_title );
+		}
+	}
+	$category_url   = get_category_link( $category_id );
+	$category_title = get_cat_name( $category_id );
+	rddgbc_print( $category_url, $category_title );
 }
 
 /**
  * This method gets current position value, returns formated string and
- * increments position value
+ * increments position value.
+ *
  * @return string
  */
 function rddgbc_get_position() {
-  $position_counter = $GLOBALS['position'];
-  $position_html    = "<meta itemprop=\"position\" content=\"{$position_counter}\">";
-  $GLOBALS['position']++;
-  return $position_html;
+	$position_counter = $GLOBALS['position'];
+	$position_html    = "<meta itemprop=\"position\" content=\"{$position_counter}\">";
+	$GLOBALS['position']++;
+	return $position_html;
 }
 
 /**
- * This method echoes current list item according to the following pattern
- * @param  string  $url
- * @param  string  $title
- * @param  boolean $is_last
+ * This method echoes current list item according to the following pattern.
+ *
+ * @param  string  $url URL of the current item.
+ * @param  string  $title Title for current item.
+ * @param  boolean $is_last Flag for last item.
  * @return void
  */
 function rddgbc_print( $url, $title, $is_last = false ) {
-  $li_opened    = '<li class="rddgbc__item" itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">';
-  $li_last      = '<li class="rddgbc__item rddgbc__item--active" aria-current="page" itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">';
-  $a_opened     = "<a class=\"rddgbc__link\" href=\"{$url}\" itemprop=\"item\" itemtype=\"http://schema.org/Thing\">";
-  $span_opened  = "<span itemprop=\"name\">";
-  $span_closed  = "</span>";
-  $a_closed     = '</a>';
-  $position     = rddgbc_get_position();
-  $li_closed    = '</li>';
-  if ( true == $is_last )
-    echo "{$li_last}{$a_opened}{$span_opened}{$title}{$span_closed}{$a_closed}{$position}{$li_closed}";
-  else
-    echo "{$li_opened}{$a_opened}{$span_opened}{$title}{$span_closed}{$a_closed}{$position}{$li_closed}";
+	$li_opened   = '<li class="rddgbc__item" itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">';
+	$li_last     = '<li class="rddgbc__item rddgbc__item--active" aria-current="page" itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">';
+	$a_opened    = "<a class=\"rddgbc__link\" href=\"{$url}\" itemprop=\"item\" itemtype=\"http://schema.org/Thing\">";
+	$span_opened = '<span itemprop="name">';
+	$span_closed = '</span>';
+	$separator   = '<span class="rddgbc__separator">&raquo;</span>';
+	$a_closed    = '</a>';
+	$position    = rddgbc_get_position();
+	$li_closed   = '</li>';
+	if ( true === $is_last ) {
+		echo "{$li_last}{$a_opened}{$span_opened}{$title}{$span_closed}{$a_closed}{$position}{$li_closed}";
+	} else {
+		echo "{$li_opened}{$a_opened}{$span_opened}{$title}{$span_closed}{$a_closed}{$separator}{$position}{$li_closed}";
+	}
 }
